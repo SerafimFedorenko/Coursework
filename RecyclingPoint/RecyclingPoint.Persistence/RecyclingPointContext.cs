ec977166734df42d8038a27e3f97e3916b6f4aa8
SerafimFedorenko <92511061+SerafimFedorenko@users.noreply.github.com>
2022-10-15 17:55:23 +0300

    .

--- RecyclingPoint/RecyclingPoint.Persistence/RecyclingPointContext.cs
+++ RecyclingPoint/RecyclingPoint.Persistence/RecyclingPointContext.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RecyclingPoint.Domain;
 
 namespace RecyclingPoint.Persistence
 {
-    internal class RecyclingPointContext
+    public class RecPointContext : DbContext
     {
+        public DbSet<Position>? Positions { get; set; }
+        public DbSet<RecyclableType>? RecyclableTypes { get; set; }
+        public DbSet<StorageType>? StorageTypes { get; set; }
+        public DbSet<Employee>? Employees { get; set; }
+        public DbSet<Storage>? Storages { get; set; }
+        public DbSet<AcceptedRecyclable>? AcceptedRecyclables { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            ConfigurationBuilder builder = new();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+            IConfigurationRoot config = builder.Build();
+            string connectionString = config.GetConnectionString("SQLConnection");
+            _ = optionsBuilder
+                .UseSqlServer(connectionString)
+                .Options;
+            optionsBuilder.LogTo(message => System.Diagnostics.Debug.WriteLine(message));
+        }
     }
 }
