7e32d86bf61de4c4a73471d9b7fbb156c5c209f6
SerafimFedorenko <92511061+SerafimFedorenko@users.noreply.github.com>
2022-11-30 10:50:04 +0300

    Добавлены базовые контроллеры и представления

---
+++ RecyclingPoint/WebApp/Controllers/AcceptedRecyclablesController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class AcceptedRecyclablesController : Controller
+    {
+        private readonly RecPointContext _context;
+
+        public AcceptedRecyclablesController(RecPointContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AcceptedRecyclables
+        public async Task<IActionResult> Index()
+        {
+            var recPointContext = _context.AcceptedRecyclables.Include(a => a.Employee).Include(a => a.RecyclableType).Include(a => a.Storage);
+            return View(await recPointContext.ToListAsync());
+        }
+
+        // GET: AcceptedRecyclables/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.AcceptedRecyclables == null)
+            {
+                return NotFound();
+            }
+
+            var acceptedRecyclable = await _context.AcceptedRecyclables
+                .Include(a => a.Employee)
+                .Include(a => a.RecyclableType)
+                .Include(a => a.Storage)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (acceptedRecyclable == null)
+            {
+                return NotFound();
+            }
+
+            return View(acceptedRecyclable);
+        }
+
+        // GET: AcceptedRecyclables/Create
+        public IActionResult Create()
+        {
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Id");
+            ViewData["RecyclableTypeId"] = new SelectList(_context.RecyclableTypes, "Id", "Id");
+            ViewData["StorageId"] = new SelectList(_context.Storages, "Id", "Id");
+            return View();
+        }
+
+        // POST: AcceptedRecyclables/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,RecyclableTypeId,EmployeeId,StorageId,Quantity,Date")] AcceptedRecyclable acceptedRecyclable)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(acceptedRecyclable);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Id", acceptedRecyclable.EmployeeId);
+            ViewData["RecyclableTypeId"] = new SelectList(_context.RecyclableTypes, "Id", "Id", acceptedRecyclable.RecyclableTypeId);
+            ViewData["StorageId"] = new SelectList(_context.Storages, "Id", "Id", acceptedRecyclable.StorageId);
+            return View(acceptedRecyclable);
+        }
+
+        // GET: AcceptedRecyclables/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.AcceptedRecyclables == null)
+            {
+                return NotFound();
+            }
+
+            var acceptedRecyclable = await _context.AcceptedRecyclables.FindAsync(id);
+            if (acceptedRecyclable == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Id", acceptedRecyclable.EmployeeId);
+            ViewData["RecyclableTypeId"] = new SelectList(_context.RecyclableTypes, "Id", "Id", acceptedRecyclable.RecyclableTypeId);
+            ViewData["StorageId"] = new SelectList(_context.Storages, "Id", "Id", acceptedRecyclable.StorageId);
+            return View(acceptedRecyclable);
+        }
+
+        // POST: AcceptedRecyclables/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,RecyclableTypeId,EmployeeId,StorageId,Quantity,Date")] AcceptedRecyclable acceptedRecyclable)
+        {
+            if (id != acceptedRecyclable.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(acceptedRecyclable);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AcceptedRecyclableExists(acceptedRecyclable.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Id", acceptedRecyclable.EmployeeId);
+            ViewData["RecyclableTypeId"] = new SelectList(_context.RecyclableTypes, "Id", "Id", acceptedRecyclable.RecyclableTypeId);
+            ViewData["StorageId"] = new SelectList(_context.Storages, "Id", "Id", acceptedRecyclable.StorageId);
+            return View(acceptedRecyclable);
+        }
+
+        // GET: AcceptedRecyclables/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.AcceptedRecyclables == null)
+            {
+                return NotFound();
+            }
+
+            var acceptedRecyclable = await _context.AcceptedRecyclables
+                .Include(a => a.Employee)
+                .Include(a => a.RecyclableType)
+                .Include(a => a.Storage)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (acceptedRecyclable == null)
+            {
+                return NotFound();
+            }
+
+            return View(acceptedRecyclable);
+        }
+
+        // POST: AcceptedRecyclables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.AcceptedRecyclables == null)
+            {
+                return Problem("Entity set 'RecPointContext.AcceptedRecyclables'  is null.");
+            }
+            var acceptedRecyclable = await _context.AcceptedRecyclables.FindAsync(id);
+            if (acceptedRecyclable != null)
+            {
+                _context.AcceptedRecyclables.Remove(acceptedRecyclable);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AcceptedRecyclableExists(int id)
+        {
+          return _context.AcceptedRecyclables.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ RecyclingPoint/WebApp/Controllers/RecyclableTypesController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class RecyclableTypesController : Controller
+    {
+        private readonly RecPointContext _context;
+
+        public RecyclableTypesController(RecPointContext context)
+        {
+            _context = context;
+        }
+
+        // GET: RecyclableTypes
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.RecyclableTypes.ToListAsync());
+        }
+
+        // GET: RecyclableTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.RecyclableTypes == null)
+            {
+                return NotFound();
+            }
+
+            var recyclableType = await _context.RecyclableTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (recyclableType == null)
+            {
+                return NotFound();
+            }
+
+            return View(recyclableType);
+        }
+
+        // GET: RecyclableTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RecyclableTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Price,Description,AcceptanceCondition,StorageCondition")] RecyclableType recyclableType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(recyclableType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(recyclableType);
+        }
+
+        // GET: RecyclableTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.RecyclableTypes == null)
+            {
+                return NotFound();
+            }
+
+            var recyclableType = await _context.RecyclableTypes.FindAsync(id);
+            if (recyclableType == null)
+            {
+                return NotFound();
+            }
+            return View(recyclableType);
+        }
+
+        // POST: RecyclableTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,Description,AcceptanceCondition,StorageCondition")] RecyclableType recyclableType)
+        {
+            if (id != recyclableType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(recyclableType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RecyclableTypeExists(recyclableType.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(recyclableType);
+        }
+
+        // GET: RecyclableTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.RecyclableTypes == null)
+            {
+                return NotFound();
+            }
+
+            var recyclableType = await _context.RecyclableTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (recyclableType == null)
+            {
+                return NotFound();
+            }
+
+            return View(recyclableType);
+        }
+
+        // POST: RecyclableTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.RecyclableTypes == null)
+            {
+                return Problem("Entity set 'RecPointContext.RecyclableTypes'  is null.");
+            }
+            var recyclableType = await _context.RecyclableTypes.FindAsync(id);
+            if (recyclableType != null)
+            {
+                _context.RecyclableTypes.Remove(recyclableType);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RecyclableTypeExists(int id)
+        {
+          return _context.RecyclableTypes.Any(e => e.Id == id);
+        }
+    }
+}
